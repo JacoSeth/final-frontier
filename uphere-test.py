e0ef90da8fb99b7af24c063340622bd1a321eadb
“Seth <seth.s.jacobson@gmail.com>
2023-01-10 09:24:21 -1000

    modified:   .gitignore 	new file:   __pycache__/pyconfig.cpython-39.pyc 	new file:   config.json 	modified:   uphere-test.py

---
+++ uphere-test.py
@@ -0,0 +1,39 @@
+# Import Packages
+
+import requests
+import json
+
+# Read Config Auth
+with open ('config.json', 'r') as config:
+    configData = json.load(config)
+
+# API call- return Sat Info
+def getSatInfo(satNumber):
+    url = f"https://uphere-space1.p.rapidapi.com/{satNumber}/43226/details"
+
+    headers = configData["auth"]["uphere"]
+
+    response = requests.request("GET", url, headers=headers)
+
+    dict_entry = json.loads(response.text)
+
+    mydict = {}
+
+    mydict.update(dict_entry)
+
+
+# API call- return Sat Location
+def getSatLocation(satNumber):
+
+    url = f"https://uphere-space1.p.rapidapi.com/satellite/{satNumber}/location"
+
+    querystring = {"units":"metric"}
+
+    headers = configData["auth"]["uphere"]
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # print(response.text)
+
+# getSatInfo(43226)
+# getSatLocation(43226)
